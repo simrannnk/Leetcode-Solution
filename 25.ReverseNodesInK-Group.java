d04fc45d3c48b1f567923cec697995a78a41e8c5
Simranjeet Kaur <ksimran1720@gmail.com>
2022-06-30 22:49:22 +0530

    update

---
+++ 25.ReverseNodesInK-Group.java
@@ -0,0 +1,73 @@
+class Solution {
+    
+    public int length(ListNode head)
+    {
+
+        ListNode curr=head;
+        int len=0;
+        while(curr!=null)
+        {
+            curr=curr.next;
+            len++;
+        }
+        
+        return len;
+    }
+    
+    public void addFirst(ListNode head)
+    {
+        
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+    if(head.next==null || k==1)
+    {
+        return head;
+    }
+        ListNode th=null;
+        ListNode tt=null;
+        
+        ListNode ph=null;
+        ListNode pt=null;
+        
+        ListNode curr=head;
+        int len=length(head);
+        while(curr!=null && len>=k)
+        {
+            int itr=k;
+            while(itr-->0)
+            {
+                ListNode forw=curr.next;
+                curr.next=th;
+                th=curr;
+                
+                if(tt==null)
+                {
+                    tt=curr;
+                }
+                curr=forw;
+            }
+            
+            if(pt==null)
+            {
+                ph=th;
+                pt=tt;
+            }
+            else{
+                pt.next=th;
+                pt=tt;
+            }
+            
+            th=null;
+            tt=null;
+            len=len-k;
+        }
+        
+      if(curr!=null)
+      {
+          pt.next=curr;
+      }
+        return ph;
+        
+    }
+}
